92a0652ce1fd378c65d8f3f48b15dcb321c273d4
Mohammad Atif <mhdatif0511@gmail.com>
2025-12-23 09:17:12 +0530

    Enhance RSS feed structure by adding content namespace to RSS definition

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -22,7 +22,11 @@ const generateRssItem = (config, post) => `
 `
 
 const generateRss = (config, posts, page = 'rss.xml') => `
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <rss
+    version="2.0"
+    xmlns:atom="http://www.w3.org/2005/Atom"
+    xmlns:content="http://purl.org/rss/1.0/modules/content/"
+  >
     <channel>
       <title>${escape(config.title)}</title>
       <link>${config.siteUrl}/blog</link>
